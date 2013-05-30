69391e14c9b367c70a8a256e1b4ceaab1c4d823c
Daisy Hernandez <dhernandez@hmc.edu>
2013-05-30 09:06:21 -0700

    Made comment modifications to the file

--- date_helpers.py
+++ date_helpers.py
@@ -1,17 +1,17 @@
 # Allison Schubauer and Daisy Hernandez
 # Created: 5/29/2013
-# Last Updated: 5/29/2013
+# Last Updated: 5/30/2013
 # For JCAP
 
 """
-Has helpful date functions to converting to datetime objects
+Has a function that helps us deal with time conversion
 """
 
 import time 
 from time import strftime
 import datetime
 
-# give a time.time() object and it returns a datetime object
+# give a float of seconds - such as time.time() and it returns a datetime obj
 def dateObj(atime):
     localCurrTime = strftime("%m/%d/%Y %H:%M:%S", time.localtime(atime))
     return datetime.datetime.strptime(localCurrTime, "%m/%d/%Y %H:%M:%S")
