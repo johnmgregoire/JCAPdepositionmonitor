82702e83ddd0bc698414c949ffa8e344f9579c81
Allison Schubauer <aschubauer@hmc.edu>
2013-05-30 09:18:15 -0700

    added comments to mainmenu

--- mainmenu.py
+++ mainmenu.py
@@ -1,24 +1,31 @@
 # Allison Schubauer and Daisy Hernandez
 # Created: 5/28/2013
-# Last Updated: 5/28/2013
+# Last Updated: 5/30/2013
 # For JCAP
 
 from graphwindow_data import *
 from profilewindow import *
 import os
 
+""" window that pops up when application launches """
 class MainMenu(QtGui.QWidget):
 
     def __init__(self):
         super(MainMenu, self).__init__()
+        # holds all active windows
         self.windows = []
+        # holds all profiles associated with current file
         self.profiles = {}
+        # save name of file from which application will read
         self.defaultFile = self.initReader()
+        # initializes reader on data file
         self.reader = DataReader(parent=self, filename=self.defaultFile)
         self.reader.start()
         
         self.initUI()
 
+    """ automatically loads last modified data file
+        when application launches """
     def initReader(self):
         targetDir = 'C:/Users/JCAP-HTE/Documents/GitHub/JCAPdepositionmonitor'
         lastModifiedFile = ''
@@ -33,6 +40,7 @@ class MainMenu(QtGui.QWidget):
                 lastModifiedFile = filename
         return lastModifiedFile
 
+    """ draws graphical user interface """
     def initUI(self):
         self.setGeometry(50, 150, 300, 400)
         self.setWindowTitle('Deposition Monitor') # or whatever this application is actually going to be called
@@ -44,18 +52,22 @@ class MainMenu(QtGui.QWidget):
         # load profile [check]
         # color wheel
 
+        # show single graph
         makeGraphButton = QtGui.QPushButton('Show Graph')
         self.layout.addWidget(makeGraphButton)
         makeGraphButton.clicked.connect(self.makeGraph)
 
+        # create a profile
         makeProfileButton = QtGui.QPushButton('Create a New Profile')
         self.layout.addWidget(makeProfileButton)
         makeProfileButton.clicked.connect(self.makeProfile)
 
+        # show a saved profile
         loadProfileButton = QtGui.QPushButton('Load a Saved Profile')
         self.layout.addWidget(loadProfileButton)
         loadProfileButton.clicked.connect(self.selectProfile)
 
+        # initialize the timer that updates all graphs in application
         timer = QtCore.QTimer(self)
         timer.timeout.connect(self.updateAll)
         # update graph every 1000 milliseconds
@@ -63,22 +75,27 @@ class MainMenu(QtGui.QWidget):
 
         self.show()
 
+    """ creates window for single graph """
     def makeGraph(self):
         graph = GraphWindow(datafile=self.defaultFile)
         self.windows += [graph]
         graph.show()
 
+    """ shows profile creator window """
     def makeProfile(self):
         profileCreator = ProfileCreator(datafile=self.defaultFile)
         self.windows += [profileCreator]
         profileCreator.show()
 
+    """ shows load profile window """
     def selectProfile(self):
         savefile = open('saved_profiles.txt', 'rb')
         menuList = []
         while True:
+            # unpickle each profile individually
             try:
                 datafile, name, varsList = pickle.load(savefile)
+                # only show profiles associated with current data file
                 if datafile == self.defaultFile:
                     self.profiles[name] = varsList
                     menuList += [name]
@@ -89,12 +106,14 @@ class MainMenu(QtGui.QWidget):
         loadMenu.show()
         loadMenu.profileChosen.connect(self.loadProfile)
 
+    """ once profile is chosen, loads profile in new window """
     def loadProfile(self, name):
         varsList = self.profiles.get(str(name))
         profileWindow = ProfileWindow(name, varsList)
         self.windows += [profileWindow]
         profileWindow.show()
 
+    """ updates all active windows every second """
     def updateAll(self):
         for window in self.windows:
             if window.isHidden():
@@ -102,12 +121,13 @@ class MainMenu(QtGui.QWidget):
             else:
                 window.updateWindow()
 
+    """ terminates reader when window is closed """
     def closeEvent(self, event):
         print "signal transmitted"
         self.reader.end()
         event.accept()
 
-
+""" main event loop """
 def main():
     app = QtGui.QApplication(sys.argv)
     menu = MainMenu()
